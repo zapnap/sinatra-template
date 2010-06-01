a48aab4067c6e634178953494af408cb229b65af
garren smith <garren.smith@gmail.com>
2010-06-01 08:28:09 +0200

    Adjusted to deploy to Heroku added .gems and set the database environment feature

--- environment.rb
+++ environment.rb
@@ -15,9 +15,11 @@ configure do
                  :url_base => 'http://localhost:4567/'
                )
 
-  DataMapper.setup(:default, "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db")
-
   # load models
   $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib")
   Dir.glob("#{File.dirname(__FILE__)}/lib/*.rb") { |lib| require File.basename(lib, '.*') }
-end
+
+               
+  DataMapper.setup(:default,(ENV["DATABASE_URL"] || "sqlite3:///#{File.expand_path(File.dirname(__FILE__))}/#{Sinatra::Base.environment}.db"))
+
+  end
