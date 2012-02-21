3f8226ef4b28a0712b6d6ac3b644b4c4da24070e
Ben Woodall <benwoody@gmail.com>
2012-02-21 15:04:46 -0800

    changed validations to reflect couchrest_model

--- lib/profile.rb
+++ lib/profile.rb
@@ -1,11 +1,11 @@
-class Profile < CouchRest::ExtendedDocument
-  include CouchRest::Validation
+class Profile < CouchRest::Model::Base
+
   use_database CouchRest.database!((SiteConfig.url_base_db || '') + SiteConfig.db_name)
 
   property :name
   timestamps!
 
-  validates_present :name
+  #validates_presence_of :name
 
   view_by :name
   # view_by :created_at, :descending => true
